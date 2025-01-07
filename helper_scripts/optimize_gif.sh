54a0d4f3a9c547d7953a29b7aceb7ef83fa94567
Stattek <stattek1@pm.me>
2025-01-07 16:48:05 -0600

    Update helper scripts and add new ones.

--- helper_scripts/optimize_gif.sh
+++ helper_scripts/optimize_gif.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 if [ "$#" -ne "3" ]; then
-    echo "Usage: convert_gif_to_pngs <fps> <gif_resolution> <output_image_name>"
+    echo "Usage: optimize_gif <input_gif_name> <fps> <gif_resolution> <output_image_name>"
     echo "Assumes that the input images are named outimage<num>.png and outputs the gif as <output_image_name>.gif"
     exit 1
 fi
 
-FPS=$(wc -l $1)
-GIF_RESOLUTION=$(wc -l $2)
-OUTPUT_IMAGE_NAME=$(wc -l <$3)
+INPUT_GIF_NAME="$1"
+FPS="$2"
+GIF_RESOLUTION="$3"
+OUTPUT_IMAGE_NAME="$4"
 
 ffmpeg -y -i final_output.gif -filter_complex "fps=$(FPS),scale=$(GIF_RESOLUTION):-1:flags=lanczos" "($OUTPUT_IMAGE_NAME).gif"
