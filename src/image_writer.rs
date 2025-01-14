820298c03cc3c2c37680af5dbdb2be2e66f7c2a7
Stattek <stattek1@pm.me>
2025-01-14 14:16:31 -0600

    Move definition into another file.

--- src/image_writer.rs
+++ src/image_writer.rs
@@ -1,29 +1,5 @@
-use std::ops::Deref;
-
-use image::{GenericImageView, ImageBuffer};
-
-/// Represents the image data to work with.
-/// Holds an `ImageBuffer` with the image data.
-#[derive(Clone)]
-pub struct ImageData(ImageBuffer<image::Rgba<u8>, Vec<u8>>);
-
-impl ImageData {
-    /// Create a new ImageData struct as this image buffer.
-    pub fn new(image_buffer: ImageBuffer<image::Rgba<u8>, Vec<u8>>) -> Self {
-        Self(image_buffer)
-    }
-}
-
-impl Deref for ImageData {
-    type Target = ImageBuffer<image::Rgba<u8>, Vec<u8>>;
-
-    /// Gets the underlying `ImageBuffer<image::Rgba<u8>, Vec<u8>>`
-    /// that this struct is a wrapper for.
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
+use crate::image_data::ImageData;
+use image::GenericImageView;
 #[derive(Clone)]
 pub struct MyImageWriter {
     pub imagebuf: ImageData,
