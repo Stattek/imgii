c1f5b0740dae8f2b0c2026c63012e791c2622fe8
Stattek <alienhuntsgames@gmail.com>
2025-02-03 00:51:30 -0600

    Fix up helper shell scripts.

--- helper_scripts/optimize_gif.sh
+++ helper_scripts/optimize_gif.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-if [ "$#" -ne "3" ]; then
-    echo "Usage: optimize_gif <input_gif_name> <fps> <gif_resolution> <output_image_name>"
-    echo "Assumes that the input images are named outimage<num>.png and outputs the gif as <output_image_name>.gif"
+if [ "$#" -ne "4" ]; then
+    echo "Usage: optimize_gif <input_gif_name> <fps> <gif_resolution> <output_gif_name>"
+    echo "    <input_gif_name> is the name of the gif to optimize."
+    echo "    <fps> is the output gif's fps."
+    echo "    <gif_resolution> is the scale of the output gif."
+    echo "    <output_gif_name> is the output gif (specify .gif at the end)"
     exit 1
 fi
 
 INPUT_GIF_NAME="$1"
 FPS="$2"
 GIF_RESOLUTION="$3"
-OUTPUT_IMAGE_NAME="$4"
+OUTPUT_GIF_NAME="$4"
 
-ffmpeg -y -i final_output.gif -filter_complex "fps=$(FPS),scale=$(GIF_RESOLUTION):-1:flags=lanczos" "($OUTPUT_IMAGE_NAME).gif"
+ffmpeg -y -i "$INPUT_GIF_NAME" -filter_complex "fps=$FPS,scale=$GIF_RESOLUTION:-1:flags=lanczos" "$OUTPUT_GIF_NAME"
