c2f51daf74ad997e167a0bd682122d19361b7ac6
Stattek <stattek1@pm.me>
2025-02-16 21:54:44 -0600

    Allow setting the background to black.

--- src/ascii_image_options.rs
+++ src/ascii_image_options.rs
@@ -4,12 +4,20 @@ use crate::render_char_to_png::DEFAULT_CHAR_FONT_SIZE;
 pub struct AsciiImageOptions {
     /// The font size of the output image.
     font_size: Option<u32>,
+
+    /// Sets a black background behind the image.
+    ///
+    /// No background by default.
+    pub background: bool,
 }
 
 impl AsciiImageOptions {
     /// Creates a new image options object.
-    pub fn new(font_size: Option<u32>) -> Self {
-        Self { font_size }
+    pub fn new(font_size: Option<u32>, background: bool) -> Self {
+        Self {
+            font_size,
+            background,
+        }
     }
 
     /// Gets the font size if present, otherwise gives back the default
--- src/render_char_to_png.rs
+++ src/render_char_to_png.rs
@@ -1,7 +1,8 @@
-use crate::image_data::ImageData;
+use crate::{ascii_image_options::AsciiImageOptions, image_data::ImageData};
 use ab_glyph::{FontRef, PxScale};
-use image::{DynamicImage, Rgba, RgbaImage};
+use image::{DynamicImage, ImageBuffer, Rgba, RgbaImage};
 use imageproc::drawing::draw_text_mut;
+use rayon::prelude::*;
 use std::u8;
 
 /// Represents a colored string to write.
@@ -15,10 +16,16 @@ pub struct ColoredStr {
 }
 
 pub const DEFAULT_CHAR_FONT_SIZE: u32 = 16;
+const BACKGROUND_PIXEL: Rgba<u8> = Rgba([0, 0, 0, u8::MAX]);
 
 /// Converts string data into a png
 /// Uses `imageproc` to render text.
-pub fn str_to_png(data: ColoredStr, font: &FontRef<'_>, font_size: u32) -> Result<ImageData, ()> {
+pub fn str_to_png(
+    data: ColoredStr,
+    font: &FontRef<'_>,
+    ascii_image_options: &AsciiImageOptions,
+) -> Result<ImageData, ()> {
+    let font_size = ascii_image_options.get_font_size();
     let (char_width, char_height) = calculate_char_dimensions(font_size);
     // create our image to work with
     let mut image = RgbaImage::new(char_width, char_height);
@@ -26,6 +33,12 @@ pub fn str_to_png(data: ColoredStr, font: &FontRef<'_>, font_size: u32) -> Resul
         x: font_size as f32,
         y: font_size as f32,
     };
+
+    // set background if user wants it
+    if ascii_image_options.background {
+        set_background(&mut image);
+    }
+
     draw_text_mut(
         &mut image,
         Rgba([data.red, data.green, data.blue, u8::MAX]),
@@ -39,10 +52,24 @@ pub fn str_to_png(data: ColoredStr, font: &FontRef<'_>, font_size: u32) -> Resul
     return Ok(ImageData::new(image));
 }
 
+fn set_background(image: &mut ImageBuffer<Rgba<u8>, Vec<u8>>) {
+    image.par_enumerate_pixels_mut().for_each(|(_, _, pixel)| {
+        // set background
+        *pixel = BACKGROUND_PIXEL;
+    });
+}
+
 /// Creates a transparent png in place of a character
-pub fn str_to_transparent_png(font_size: u32) -> ImageData {
-    let (char_width, char_height) = calculate_char_dimensions(font_size);
-    ImageData::new(DynamicImage::new_rgba8(char_width, char_height).into())
+pub fn str_to_transparent_png(ascii_image_options: &AsciiImageOptions) -> ImageData {
+    let (char_width, char_height) = calculate_char_dimensions(ascii_image_options.get_font_size());
+    let mut output = DynamicImage::new_rgba8(char_width, char_height).into();
+
+    // set background if user wants it
+    if ascii_image_options.background {
+        set_background(&mut output);
+    }
+
+    ImageData::new(output)
 }
 
 /// Calculates character dimensions are returns them
